61cda83b635e5643d7356461e6bffd4b18e62bfe
Reyhana Izzal muttaqim <izzalm.008@gmail.com>
2025-12-26 01:24:48 +0700

    feat: add table program tercher

---
+++ app/Models/ProgramTeacher.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProgramTeacher extends Model
+{
+    use HasFactory;
+    protected $table = 'program_teachers';
+
+    protected $fillable = [
+        'program_id',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function program(): BelongsTo
+    {
+        return $this->belongsTo(Program::class);
+    }
+}
